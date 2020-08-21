0efff88de21f2204686692a7a2c910529f2a672f
eyabc <bey4314@naver.com>
2020-08-21 15:56:06 +0900

    fix: Link class 속성 제거

--- components/user/UserHeader.js
+++ components/user/UserHeader.js
@@ -12,12 +12,13 @@ const UserHeader = () => {
     const logout = async () => {
         setUser(undefined);
         setPageTitle(undefined);
+        localStorage.clear();
         alert('로그아웃 되었습니다.');
         await Router.push('/');
     };
     return (
         <>
-            <Link href={ '/my-profile' } class={ 'my-profile' }>
+            <Link href={ '/my-profile'}>
                 <button onClick={() => setPageTitle('내정보')}>내정보</button>
             </Link>
             <button onClick={logout}>로그아웃</button>
