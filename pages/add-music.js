b8d12355826e2b595d50ec3740e9cf25452b8c62
eyabc <bey4314@naver.com>
2020-08-21 23:13:00 +0900

    feat: 음원 추가

--- pages/add-music.js
+++ pages/add-music.js
@@ -3,7 +3,8 @@ import { useRecoilState } from 'recoil/dist';
 import { pageTitleAtom } from '../recoil/common';
 import { useState } from 'react';
 import * as musicMetadata from 'music-metadata-browser';
-
+import utils, { trackParse } from '../utils';
+import Router from 'next/router'
 
 const AddMusic = () => {
     const [, setPageTitle] = useRecoilState(pageTitleAtom);
@@ -33,14 +34,33 @@ const AddMusic = () => {
         setAlbum(common.album);
         setTrack(common.track);
         setArtist(common.artist);
+    };
 
-        console.log(await musicMetadata.parseBlob(file));
+    const uploadMusic = async (event) => {
+        event.preventDefault();
+
+        try {
+            const body = new FormData();
+            body.append('title', musicTitle);
+            body.append('album', album);
+            body.append('track', JSON.stringify(track));
+            body.append('artist', artist);
+            body.append('file', musicFile);
+            await utils.client().post('/music', body);
+
+            alert('음원이 추가되었습니다!');
+            setPageTitle(undefined);
+            await Router.push('/');
+
+        } catch (error) {
+            console.log(error);
+        }
     };
 
 
     return (
         <>
-            <form action="">
+            <form action="post" onSubmit={ uploadMusic }>
                 <ul>
                     <li>
                         <label htmlFor={ 'music-file' }>음원 삽입</label>
@@ -52,28 +72,39 @@ const AddMusic = () => {
                         <button onClick={ deleteMusicFile }>삭제</button>
                     </li>
                     <li>
-                        <label htmlFor={'music-info'}>음원 정보</label>
-                        <p>트랙: { track.no && track.no } {track.of && ' of ' + track.of } </p>
-                        <p>앨범명: { album }</p>
+                        <label htmlFor={ 'music-info' }>음원 정보</label>
+                        <ul name={ 'music-info' }>
+                            <li>
+                                <label htmlFor={ 'track' }>
+                                </label>
+                                <span name={ 'track' }>
+                                    트랙: { trackParse(track)}
+                                </span>
+                            </li>
+                            <li>
+                                <label htmlFor={ 'album' }></label>
+                                <span name={ 'album' }>앨범명: { album }</span>
+                            </li>
+                        </ul>
                     </li>
                     <li>
-                        <label htmlFor={'title'}>곡 제목</label>
+                        <label htmlFor={ 'title' }>곡 제목</label>
                         <input type="text"
-                               name={'title'}
+                               name={ 'title' }
                                value={ musicTitle }
                                onChange={ changeMusicTitle }
                                required/>
                     </li>
                     <li>
-                        <label htmlFor={'artist'}>아티스트</label>
+                        <label htmlFor={ 'artist' }>아티스트</label>
                         <input type="text"
-                               name={'artist'}
+                               name={ 'artist' }
                                value={ artist }
                                onChange={ changeArtist }
                                required/>
                     </li>
                 </ul>
-                <button>완료</button>
+                <button type={ 'submit' }>완료</button>
             </form>
             <Link href={ '/' }>
                 <button onClick={ () => setPageTitle(undefined) }>목록 으로</button>
